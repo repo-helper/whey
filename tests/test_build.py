6f09bd1c7a25167c8aa55d3bda630b2c501016fe
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-02-22 09:44:41 +0000

    Refactor tests.

--- tests/test_build.py
+++ tests/test_build.py
@@ -7,83 +7,20 @@ import zipfile
 import pytest
 from coincidence.regressions import AdvancedDataRegressionFixture
 from domdf_python_tools.paths import PathPlus
-from pytest_regressions.file_regression import FileRegressionFixture
 
 # this package
+from tests.example_configs import (
+		AUTHORS,
+		CLASSIFIERS,
+		DEPENDENCIES,
+		ENTRY_POINTS,
+		KEYWORDS,
+		MAINTAINERS,
+		MINIMAL_CONFIG,
+		OPTIONAL_DEPENDENCIES,
+		URLS
+		)
 from whey import SDistBuilder, WheelBuilder
-
-MINIMAL_CONFIG = '[project]\nname = "spam"\nversion = "2020.0.0"'
-
-KEYWORDS = f"""\
-{MINIMAL_CONFIG}
-keywords = ["egg", "bacon", "sausage", "tomatoes", "Lobster Thermidor"]
-"""
-
-AUTHORS = f"""\
-{MINIMAL_CONFIG}
-authors = [
-  {{email = "hi@pradyunsg.me"}},
-  {{name = "Tzu-Ping Chung"}}
-]
-"""
-
-MAINTAINERS = f"""\
-{MINIMAL_CONFIG}
-maintainers = [
-  {{name = "Brett Cannon", email = "brett@python.org"}}
-]
-"""
-
-CLASSIFIERS = f"""\
-{MINIMAL_CONFIG}
-classifiers = [
-  "Development Status :: 4 - Beta",
-  "Programming Language :: Python"
-]
-"""
-
-DEPENDENCIES = f"""\
-{MINIMAL_CONFIG}
-dependencies = [
-  "httpx",
-  "gidgethub[httpx]>4.0.0",
-  "django>2.1; os_name != 'nt'",
-  "django>2.0; os_name == 'nt'"
-]
-"""
-
-OPTIONAL_DEPENDENCIES = f"""\
-{MINIMAL_CONFIG}
-
-[project.optional-dependencies]
-test = [
-  "pytest < 5.0.0",
-  "pytest-cov[all]"
-]
-"""
-
-URLS = f"""\
-{MINIMAL_CONFIG}
-
-[project.urls]
-homepage = "example.com"
-documentation = "readthedocs.org"
-repository = "github.com"
-changelog = "github.com/me/spam/blob/master/CHANGELOG.md"
-"""
-
-ENTRY_POINTS = f"""\
-{MINIMAL_CONFIG}
-
-[project.scripts]
-spam-cli = "spam:main_cli"
-
-[project.gui-scripts]
-spam-gui = "spam:main_gui"
-
-[project.entry-points."spam.magical"]
-tomatoes = "spam:main_tomatoes"
-"""
 
 COMPLETE_PROJECT_A = """\
 [project]
--- tests/test_config.py
+++ tests/test_config.py
@@ -10,80 +10,18 @@ from domdf_python_tools.compat import PYPY37
 from domdf_python_tools.paths import PathPlus
 
 # this package
+from tests.example_configs import (
+		AUTHORS,
+		CLASSIFIERS,
+		DEPENDENCIES,
+		ENTRY_POINTS,
+		KEYWORDS,
+		MAINTAINERS,
+		MINIMAL_CONFIG,
+		OPTIONAL_DEPENDENCIES,
+		URLS
+		)
 from whey.config import AbstractConfigParser, BadConfigError, construct_path, load_toml
-
-MINIMAL_CONFIG = '[project]\nname = "spam"\nversion = "2020.0.0"'
-
-KEYWORDS = f"""\
-{MINIMAL_CONFIG}
-keywords = ["egg", "bacon", "sausage", "tomatoes", "Lobster Thermidor"]
-"""
-
-AUTHORS = f"""\
-{MINIMAL_CONFIG}
-authors = [
-  {{email = "hi@pradyunsg.me"}},
-  {{name = "Tzu-Ping Chung"}}
-]
-"""
-
-MAINTAINERS = f"""\
-{MINIMAL_CONFIG}
-maintainers = [
-  {{name = "Brett Cannon", email = "brett@python.org"}}
-]
-"""
-
-CLASSIFIERS = f"""\
-{MINIMAL_CONFIG}
-classifiers = [
-  "Development Status :: 4 - Beta",
-  "Programming Language :: Python"
-]
-"""
-
-DEPENDENCIES = f"""\
-{MINIMAL_CONFIG}
-dependencies = [
-  "httpx",
-  "gidgethub[httpx]>4.0.0",
-  "django>2.1; os_name != 'nt'",
-  "django>2.0; os_name == 'nt'"
-]
-"""
-
-OPTIONAL_DEPENDENCIES = f"""\
-{MINIMAL_CONFIG}
-
-[project.optional-dependencies]
-test = [
-  "pytest < 5.0.0",
-  "pytest-cov[all]"
-]
-"""
-
-URLS = f"""\
-{MINIMAL_CONFIG}
-
-[project.urls]
-homepage = "example.com"
-documentation = "readthedocs.org"
-repository = "github.com"
-changelog = "github.com/me/spam/blob/master/CHANGELOG.md"
-"""
-
-ENTRY_POINTS = f"""\
-{MINIMAL_CONFIG}
-
-[project.scripts]
-spam-cli = "spam:main_cli"
-
-[project.gui-scripts]
-spam-gui = "spam:main_gui"
-
-[project.entry-points."spam.magical"]
-tomatoes = "spam:main_tomatoes"
-"""
 
 COMPLETE_PROJECT_A = """\
 [project]

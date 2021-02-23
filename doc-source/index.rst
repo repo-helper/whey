aedf90c425fea72446acd8b1cedc2304d760a97e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-02-23 08:07:24 +0000

    Bump version v0.0.0 -> v0.0.1

--- doc-source/index.rst
+++ doc-source/index.rst
@@ -103,7 +103,7 @@ whey
 	:alt: GitHub top language
 
 .. |commits-since| github-shield::
-	:commits-since: v0.0.0
+	:commits-since: v0.0.1
 	:alt: GitHub commits since tagged version
 
 .. |commits-latest| github-shield::
--- whey/__init__.py
+++ whey/__init__.py
@@ -40,7 +40,7 @@ __all__ = ["build_sdist", "build_wheel", "SDistBuilder", "WheelBuilder"]
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.0.0"
+__version__: str = "0.0.1"
 __email__: str = "dominic@davis-foster.co.uk"
 
 

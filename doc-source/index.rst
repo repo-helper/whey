0c6d227fa4e5ce7bfc100b5d42df37dde1db7e00
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-04-21 14:30:08 +0100

    Bump version v0.0.20 -> v0.0.21

--- doc-source/index.rst
+++ doc-source/index.rst
@@ -135,7 +135,7 @@ whey
 		:alt: GitHub top language
 
 	.. |commits-since| github-shield::
-		:commits-since: v0.0.20
+		:commits-since: v0.0.21
 		:alt: GitHub commits since tagged version
 
 	.. |commits-latest| github-shield::
--- whey/__init__.py
+++ whey/__init__.py
@@ -29,7 +29,7 @@ A simple Python wheel builder for simple projects.
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.0.20"
+__version__: str = "0.0.21"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = ["build_sdist", "build_wheel"]

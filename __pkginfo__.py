758ef46e593d6c6047c4d1b6e6e19585383a5968
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-05-18 22:58:09 +0100

    Bump pyproject-parser version.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -3,7 +3,7 @@
 __all__ = ["extras_require"]
 
 extras_require = {
-		"readme": ["docutils==0.16", "pyproject-parser[readme]>=0.6.0b2"],
+		"readme": ["docutils==0.16", "pyproject-parser[readme]>=0.6.0"],
 		"editable": ["editables>=0.2"],
-		"all": ["docutils==0.16", "editables>=0.2", "pyproject-parser[readme]>=0.6.0b2"]
+		"all": ["docutils==0.16", "editables>=0.2", "pyproject-parser[readme]>=0.6.0"]
 		}

1bbc4f136485d9f81746f0662241487843c394d7
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-12-10 15:20:26 +0000

    Cap docutils version; broken with newer ones.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -3,7 +3,7 @@
 __all__ = ["extras_require"]
 
 extras_require = {
-		"readme": ["docutils>=0.16", "pyproject-parser[readme]>=0.11.0b1"],
+		"readme": ["docutils<0.22,>=0.16", "pyproject-parser[readme]>=0.11.0b1"],
 		"editable": ["editables>=0.2"],
-		"all": ["docutils>=0.16", "editables>=0.2", "pyproject-parser[readme]>=0.11.0b1"]
+		"all": ["docutils<0.22,>=0.16", "editables>=0.2", "pyproject-parser[readme]>=0.11.0b1"]
 		}

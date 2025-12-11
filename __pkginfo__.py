3a74224eb2c799b8412b998b4793007bbfc5a24d
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-12-11 23:11:26 +0000

    Cap nh3 on older PyPys with [readme] extra.
    Newer version can't be built there but pip tries to anyway.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -3,7 +3,16 @@
 __all__ = ["extras_require"]
 
 extras_require = {
-		"readme": ["docutils<0.22,>=0.16", "pyproject-parser[readme]>=0.11.0b1"],
+		"readme": [
+				"docutils<0.22,>=0.16",
+				'nh3<0.3.2; platform_python_implementation == "PyPy" and python_version < "3.11"',
+				"pyproject-parser[readme]>=0.11.0b1"
+				],
 		"editable": ["editables>=0.2"],
-		"all": ["docutils<0.22,>=0.16", "editables>=0.2", "pyproject-parser[readme]>=0.11.0b1"]
+		"all": [
+				"docutils<0.22,>=0.16",
+				"editables>=0.2",
+				'nh3<0.3.2; platform_python_implementation == "PyPy" and python_version < "3.11"',
+				"pyproject-parser[readme]>=0.11.0b1"
+				]
 		}
